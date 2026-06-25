a127b1d93717637d20b3476df52010f06167e649
manishmanjul47 <>
2026-06-25 06:20:31 +0000

    Challenge: Basic Method Creation

---
+++ section/challenge_basic_method_creation/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+    public static void printWelcome() {
+        // TODO: Print the welcome message
+        System.out.println("Welcome to the Java Methods Challenge!");
+    }
+
+    public static void showInstructions() {
+        // TODO: Print the instructions message
+        System.out.println("Follow the instructions carefully to complete the task.");    }
+
+    public static void printCompletion() {
+        // TODO: Print the completion message
+        System.out.println("Congratulations! You have completed the challenge.");
+    }
+
+    public static void main(String[] args) {
+        printWelcome();
+        showInstructions();
+        printCompletion();
+    }
+}
